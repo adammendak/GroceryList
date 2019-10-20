32e4813b4c02c2b724bbbe2fb3e7181c859eefc3
adammendak <adam.mendak@gmail.com>
2019-10-20 14:07:53 +0200

    more

--- core/src/main/java/com/grocerylist/core/controller/DishCategoryConroller.java
+++ core/src/main/java/com/grocerylist/core/controller/DishCategoryConroller.java
@@ -2,9 +2,11 @@ package com.grocerylist.core.controller;
 
 import com.grocerylist.core.service.DishCategoryService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/dishCategory")
 @RequiredArgsConstructor
 public class DishCategoryConroller {
 
---
+++ core/src/main/java/com/grocerylist/core/service/AuthenticationService.java
@@ -0,0 +1,8 @@
+package com.grocerylist.core.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthenticationService {
+    //todo implement user and admin authentication
+}
