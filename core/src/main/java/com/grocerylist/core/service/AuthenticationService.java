1360cec8a4063fed74d15d0486e5c5142a81a7ca
adammendak <adam.mendak@gmail.com>
2019-11-09 11:11:47 +0100

    changes...

--- core/src/main/java/com/grocerylist/core/service/AuthenticationService.java
+++
@@ -1,8 +0,0 @@
-package com.grocerylist.core.service;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public class AuthenticationService {
-    //todo implement user and admin authentication
-}
