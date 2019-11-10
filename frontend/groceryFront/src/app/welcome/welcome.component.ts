b686dc6642426e2f4408b0ced25ef38601530992
adammendak <adam.mendak@gmail.com>
2019-11-10 14:39:51 +0100

    more

--- frontend/groceryFront/src/app/welcome/welcome.component.ts
+++ frontend/groceryFront/src/app/welcome/welcome.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WelcomeComponent implements OnInit {
 
+  productImage = '/assets/groceryList.png';
+
   constructor() { }
 
   ngOnInit() {
