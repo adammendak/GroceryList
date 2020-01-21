2363bb3a21101be94cbcaded26724631d2022e77
adammendak <adam.mendak@gmail.com>
2020-01-21 19:23:26 +0100

    edit status

---
+++ frontend/groceryFront/src/app/products/edit-product-category/edit-product-category.component.ts
@@ -0,0 +1,33 @@
+import { Component } from '@angular/core';
+import { ProductService } from "../product.service";
+import { ActivatedRoute, Router } from "@angular/router";
+import {ProductCategory} from "../productCategory";
+
+@Component({
+  selector: 'app-edit-product-category',
+  templateUrl: './edit-product-category.component.html'
+})
+export class EditProductCategoryComponent {
+
+  productCategory: ProductCategory;
+  id: number;
+
+  constructor(private _route: ActivatedRoute,
+              private _productService: ProductService,
+              private _router: Router) {
+    this.id = this._route.snapshot.params['id'];
+    this._productService.getProductCategory(this._route.snapshot.params['id'])
+      .subscribe( (data) => {
+        this.productCategory = data;
+      })
+  }
+
+  cancel(): void {
+    this._router.navigate(['/product']).catch();
+  }
+
+  editProductCategory(productCategoryForm) {
+    this._productService.editCategoryForm(productCategoryForm.productCategory, this.id);
+  }
+
+}
