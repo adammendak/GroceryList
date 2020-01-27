15bc3c201cf820c2ff8f9e95938012a44c6273bd
adammendak <adam.mendak@gmail.com>
2020-01-27 21:42:58 +0100

    more

--- core/src/main/java/com/grocerylist/core/config/WebConfig.java
+++ core/src/main/java/com/grocerylist/core/config/WebConfig.java
@@ -1,16 +1,10 @@
 package com.grocerylist.core.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @Configuration
-@EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
 
     @Override
@@ -19,20 +13,6 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedOrigins("*")
                 .allowedHeaders("*");
-    }
-
-    @Bean
-    public ViewResolver getViewResolver() {
-        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
-        resolver.setPrefix("/static/");
-        resolver.setSuffix(".html");
-        return resolver;
-    }
-
-    @Override
-    public void configureDefaultServletHandling(
-            DefaultServletHandlerConfigurer configurer) {
-        configurer.enable();
     }
 
 }
--- core/src/main/java/com/grocerylist/core/controller/HomeController.java
+++ core/src/main/java/com/grocerylist/core/controller/HomeController.java
@@ -1,12 +1,14 @@
 package com.grocerylist.core.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalTime;
 
-@RestController
+@RestController()
 public class HomeController {
 
+    @GetMapping
     public String index() {
         return "Server works: " + LocalTime.now();
     }
