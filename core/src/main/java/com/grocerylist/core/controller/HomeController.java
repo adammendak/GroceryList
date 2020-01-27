0c471a24df3be0488dc8e7e3e90e798dd24f8c1c
adammendak <adam.mendak@gmail.com>
2020-01-27 21:39:03 +0100

    more

--- core/src/main/java/com/grocerylist/core/controller/HomeController.java
+++ core/src/main/java/com/grocerylist/core/controller/HomeController.java
@@ -1,14 +1,14 @@
 package com.grocerylist.core.controller;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+import java.time.LocalTime;
+
+@RestController
 public class HomeController {
 
-    @RequestMapping("/")
     public String index() {
-        return "index";
+        return "Server works: " + LocalTime.now();
     }
 }
 
